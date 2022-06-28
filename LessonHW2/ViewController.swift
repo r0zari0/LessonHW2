743be71c68c077822347b77a338bd16e5f429768
r0zari0 <maksymstovolos2002@gmail.com>
2022-06-28 13:20:01 -0400

    Initial Commit

---
+++ LessonHW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LessonHW2
+//
+//  Created by Max Stovolos on 6/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
